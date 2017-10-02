86be36151b43b9e5b85682434b27468f81729c55
Ozcan Ovunc <ozcan.ovunc@smartface.io>
2017-10-02 09:25:52 +0000

    Improve pgMainLookbook

--- scripts/pages/pgMainLookbook.js
+++ scripts/pages/pgMainLookbook.js
@@ -1,18 +1,21 @@
 const extend = require('js-base/core/extend');
 const PgMainLookbookDesign = require('ui/ui_pgMainLookbook');
+const Router = require("sf-core/ui/router");
+const System = require('sf-core/device/system');
 const pageContextPatch = require("../context/pageContextPatch");
 
 const PgMainLookbook = extend(PgMainLookbookDesign)(
   // Constructor
   function(_super) {
-    // Initalizes super class for this page scope
     _super(this);
-    
-    // overrides super.onShow method
     this.onShow = onShow.bind(this, this.onShow.bind(this));
+    this.flHeaderLeft.onTouchEnded = function() {
+      Router.goBack();
+    };
+
     pageContextPatch(this, "pgMainLookbook");
   });
-  
+
 /**
  * @event onShow
  * This event is called when a page appears on the screen (everytime).
@@ -20,7 +23,15 @@ const PgMainLookbook = extend(PgMainLookbookDesign)(
  * @param {Object} routeData - parameters passed from Router.go function
  */
 function onShow(superOnShow, routeData) {
+  const page = this;
   superOnShow();
+
+  if (System.OS === "iOS") {
+    page.flStatusBarBg.height = page.statusBar.height;
+  }
+  else {
+    page.layout.removeChild(page.flStatusBarBg);
+  }
 }
 
 module && (module.exports = PgMainLookbook);
