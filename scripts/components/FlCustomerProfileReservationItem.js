ce71c0f77581dc77c9cfabb07d10f6d6721536eb
Ozcan Ovunc <ozcan.ovunc@smartface.io>
2017-10-03 10:29:16 +0000

    Fix undefined details - profile page

--- scripts/components/FlCustomerProfileReservationItem.js
+++ scripts/components/FlCustomerProfileReservationItem.js
@@ -1,22 +1,16 @@
-/* 
-		You can modify its contents.
-*/
 const extend = require('js-base/core/extend');
-
 const FlCustomerProfileReservationItemDesign = require('library/FlCustomerProfileReservationItem');
 
 const FlCustomerProfileReservationItem = extend(FlCustomerProfileReservationItemDesign)(
   //constructor
   function(_super, props, opt) {
-    // initalizes super class for this scope
     opt = opt || {};
     _super(this, props || FlCustomerProfileReservationItemDesign.defaults);
 
     this.children.lblDate.text = new Date(opt.date).toLocaleDateString();
-    this.children.lblInfo.text = opt.type;
+    this.children.lblInfo.text = opt.name || opt.type;
     this.children.lblStatus.text = opt.status;
   }
-
 );
 
 module && (module.exports = FlCustomerProfileReservationItem);
