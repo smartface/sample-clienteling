f0455129690fbc95843ce8e5655e0d1d9f8ae5c7
Ozcan Ovunc <ozcan.ovunc@smartface.io>
2017-10-03 09:58:18 +0000

    Fix

--- scripts/lib/adjustHeaderBar.js
+++ scripts/lib/adjustHeaderBar.js
@@ -1,9 +1,11 @@
 const System = require('sf-core/device/system');
+
 module.exports = exports = function(page) {
     if (System.OS === "iOS") {
         page.flStatusBarBg && (page.flStatusBarBg.height = page.statusBar.height);
     }
     else {
+        page.headerBar.visible = false;
         page.flStatusBarBg && page.layout.removeChild(page.flStatusBarBg);
     }
 };
