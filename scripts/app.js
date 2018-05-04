5673787047248612fe2d7fb8645a492d30ee0356
ozcanovunc <ubuntu@dfc2eabdf236>
2018-05-04 07:38:18 +0000

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,4 @@
-require("./theme");
-
-require("./theme");
-
 /* globals lang */
-require("i18n/i18n.js");
-
 const Application = require("sf-core/application");
 Application.onUnhandledError = function(e) {
   alert({
@@ -13,29 +7,17 @@ Application.onUnhandledError = function(e) {
   });
 };
 
+require("i18n/i18n.js");
 require("sf-extension-utils");
+require("./context/pageContext");
+require("./theme");
 
 const Router = require("sf-core/ui/router");
 const System = require("sf-core/device/system");
 const isTablet = require("./lib/isTablet");
 
-require("./context/pageContext");
-require("./theme");
-
-// const settings = require("./settings.json");
-
-// Deprecated
-// var stylerBuilder = require("library/styler-builder");
-
-// stylerBuilder.registerThemes(themeSettings.themes || "defaultTheme");
-// stylerBuilder.setActiveTheme(themeSettings.currentTheme);
-// ------------
-
-var sliderDrawer;
-
 if (System.OS === "iOS") {
-  sliderDrawer = require("./sliderDrawer");
-  Router.sliderDrawer = sliderDrawer;
+  Router.sliderDrawer = require("./sliderDrawer");
 }
 
 Router.add("pgDashboard", require("./pages/pgDashboard"), true);
@@ -46,15 +28,12 @@ Router.add("pgLookbook", require("./pages/pgLookbook"), true);
 Router.add("pgWomen", require("./pages/pgWomen"), true);
 Router.add("pgCustomerProfile", require("./pages/pgCustomerProfile"), true);
 Router.add("pgShoppingBag", require("./pages/pgShoppingBag"), true);
-
 Router.go("pgSignup" + (isTablet ? "Tablet" : "Phone"), {
-/* Router.go("pgCustomerProfile", { */
   appStart: true
-}); 
+});
 
 if (System.OS === "Android") {
-  sliderDrawer = require("./sliderDrawer");
-  Router.sliderDrawer = sliderDrawer;
+  Router.sliderDrawer = require("./sliderDrawer");
 }
 
 // mcs.launch().then(() => {
